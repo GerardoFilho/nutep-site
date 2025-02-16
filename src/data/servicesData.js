89f64f675ac2b6e0172874f5301d9c57c487a2f5
Gerardo Filho <gerardo_filho95@hotmail..com>
2025-02-16 11:47:32 -0300

    adição das imagens da tela de serviços

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,30 +1,43 @@
+import Medico1 from "./../assets/images/Medico-1.png";
+import Medico2 from "./../assets/images/Medico-2.png";
+import Medico3 from "./../assets/images/Medico-3.png";
+import Medico4 from "./../assets/images/Medico-4.png";
+
+import Reabilitacao1 from "./../assets/images/Reabilitacao-1.png";
+import Reabilitacao2 from "./../assets/images/Reabilitacao-2.png";
+import Reabilitacao3 from "./../assets/images/Reabilitacao-3.png";
+import Reabilitacao4 from "./../assets/images/Reabilitacao-4.png";
+import Reabilitacao5 from "./../assets/images/Reabilitacao-5.png";
+import Reabilitacao6 from "./../assets/images/Reabilitacao-6.png";
+
+import Assistencia1 from "./../assets/images/Assistencia-1.png";
+import Assistencia2 from "./../assets/images/Assistencia-2.png";
+
+import Ortopedica from "./../assets/images/Ortopedica-1.png";
+
 export const medicalServices = [
   {
     id: 1,
     title: "Neuropediatria",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Medico1,
     description: "Descrição do serviço de Neuropediatria.",
   },
   {
     id: 2,
     title: "Ortopedia",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Medico2,
     description: "Descrição do serviço de Ortopedia.",
   },
   {
     id: 3,
     title: "Oftamologia",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Medico3,
     description: "Descrição do serviço de Oftamologia.",
   },
   {
     id: 4,
     title: "Otorrinolaringologia",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Medico4,
     description: "Descrição do serviço de Otorrinolaringologia.",
   },
 ];
@@ -33,43 +46,37 @@ export const rehabilitationServices = [
   {
     id: 4,
     title: "Enfermagem",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao1,
     description: "Descrição do serviço de Enfermagem.",
   },
   {
     id: 5,
     title: "Fisioterapia Neurofuncional",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao2,
     description: "Fisioterapia voltada para reabilitação.",
   },
   {
     id: 6,
     title: "Fisioterapia Respiratória",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao3,
     description: "Auxílio na melhora da função respiratória.",
   },
   {
     id: 7,
     title: "Psicologia",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao4,
     description: "Apoio psicológico ao paciente.",
   },
   {
     id: 8,
     title: "Terapia Ocupacional",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao5,
     description: "Auxílio no desenvolvimento e reabilitação.",
   },
   {
     id: 9,
     title: "Terapia Ocupacional",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Reabilitacao6,
     description: "Auxílio no desenvolvimento e reabilitação.",
   },
 ];
@@ -78,15 +85,13 @@ export const socialAssistanceServices = [
   {
     id: 10,
     title: "Assistência Social",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Assistencia1,
     description: "Serviços de assistência social.",
   },
   {
     id: 11,
     title: "Atendimento Social",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Assistencia2,
     description: "Apoio e acompanhamento familiar.",
   },
 ];
@@ -95,8 +100,7 @@ export const orthopedicWorkshop = [
   {
     id: 12,
     title: "Tecnologia Assistiva",
-    image:
-      "https://www.iped.com.br/_upload/galleries/2015/05/19/organizao-pblica-555b72de0abc9.jpg",
+    image: Ortopedica,
     description: "Confecção e manutenção de aparelhos ortopédicos.",
   },
 ];
